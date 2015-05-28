1e3af85d58f719a549ddf407712ea738618f3abd
Ansonwolfe <ansonwolfe@gmail.com>
2015-05-28 15:49:27 -0700

    added hidden / display form fields on contact form

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// hide & display extra fields on contact modal
+
+$(document).ready(function() { 
+
+	$("#hideFields").hide();
+
+	$( "#template-contactform-phone" ).focusin(function() {
+	  $( "#hideFields" ).slideDown( "slow", function() {
+	    // Animation complete.
+	  });
+	});
+
+ });
