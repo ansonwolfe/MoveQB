e46062b11fe2bd532f25ec3e0acfb8a5db914e85
abdodev <services.naji@gmail.com>
2015-12-03 16:09:52 +0000

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['address']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['persona'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$email_subject = 'New message from your website';
+$option = $_POST['persona'];
+$phone = $_POST['phone'];
+$company = $_POST['address'];
+// Create the email and send the message
+$to = 'info@greeniphi.com';
+$email_body = "You have received a new message from your website contact form.\n\nHere are the details:\n\nName: $name\n\nEmail: $email_address\n\nCompany: $company\n\nPhone: $phone\n\nWhat best describe you ?: $option";
+$headers = "From: contact@greeniphi.com\n"; 
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+echo "SENDING";
+
+return true;			
+?>
